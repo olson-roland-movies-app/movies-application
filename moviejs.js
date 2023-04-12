388ff95ab872daab30b21503694818089b438242
Jim Olson <jim.olson.456@gmail.com>
2023-04-12 11:10:15 -0500

    update

--- moviejs.js
+++ moviejs.js
@@ -1,5 +1,3 @@
-
-
 fetch("http://localhost:3000/movies")
     .then(response => response.json())
     .then(movieData => {
@@ -14,12 +12,12 @@ fetch("http://localhost:3000/movies")
 <p>${movie.rating}</p>
 </div>`
         })
-        document.getElementsByTagName("body")
-            [0].innerHTML=moviesHTML.join("");
+        document.getElementById("movie-content")
+            .innerHTML=moviesHTML.join("");
 
 
     })
-    .catch (error => console.error(error));
+    .catch (error => console.log(error));
 
 
 
@@ -29,15 +27,15 @@ fetch("http://localhost:3000/movies")
 
 
 fetch("http://localhost:3000/movies")
-   .then(response => response.json())
-   .then(data => console.log(data))
-   .catch (error => console.error(error));
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch (error => console.log(error));
 
 
 function addMovie(title, rating){
-   // title = prompt("Add A Movie")
-   // rating = prompt("Give Your Movie A Rating")
-   // return {title, rating};
+    // title = prompt("Add A Movie")
+    // rating = prompt("Give Your Movie A Rating")
+    // return {title, rating};
 }
 
 // console.log(addMovie())
@@ -45,25 +43,88 @@ function addMovie(title, rating){
 
 
 fetch("http://localhost:3000/movies",{
-   method: 'POST',
-   headers:{
-       'Content-Type' : 'application/json'
-   },
-   body: JSON.stringify(addMovie())
+    method: 'POST',
+    headers:{
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(addMovie())
 
-}) .then(response => response.json(response))
-   .then(data => console.log(data))
-   .catch (error => console.error(error));
+}) .then(response => response.json())
+    .then(data => console.log(data))
+    .catch (error => console.log(error));
 
 function deleteMovie(id) {
-   fetch(`http://localhost:3000/movies/${id}`, {
-       method: 'DELETE',
-   }).then(response => response.json(response))
-       .then(data => console.log(data))
-       .catch (error => console.error(error));
+    fetch(`http://localhost:3000/movies/${id}`, {
+        method: 'DELETE',
+    }).then(response => response.json())
+        .then(data => console.log(data))
+        .catch (error => console.log(error));
 }
 
-deleteMovie(4)
+deleteMovie(11)
+
+
+// {
+//     $(document).ready(function () {
+//         // Display loading message
+//         $('#loading-message').show();
+//
+//         // Get all movies
+//         fetch({
+//             url: 'https://localhost:3000/movies',
+//             method: 'GET',
+//             success: function (response) {
+//                 // Remove loading message
+//                 $('#loading-message').hide();
+//
+//                 // Generate HTML for movies list
+//                 let moviesHtml = '';
+//                 for (let i = 0; i < response.length; i++) {
+//                     let movie = response[i];
+//                     moviesHtml += '<div class="movie" data-id="' + movie.id + '">';
+//                     moviesHtml += '<h3>' + movie.title + '</h3>';
+//                     moviesHtml += '<p>Rating: ' + movie.rating + '</p>';
+//                     moviesHtml += '<button class="edit-movie">Edit</button>';
+//                     moviesHtml += '<button class="delete-movie">Delete</button>';
+//                     moviesHtml += '</div>';
+//                 }
+//                 $('#movies-list').html(moviesHtml);
+//             },
+//             // error: function () {
+//             //     alert('An error occurred while fetching movie list.');
+//             // }
+//         });
+//
+//
+//         // Add movie form submission
+//         $('#add-movie-form form').submit(function (event) {
+//             event.preventDefault();
+//             let title = $('#add-movie-form #title').val();
+//             let rating = $('#add-movie-form #rating').val();
+//                 fetch({
+//                 url: 'https://localhost:3000/movies',
+//                 method: 'POST',
+//                 data: {title: title, rating: rating},
+//                 success: function (response) {
+//                     // Append new movie to list
+//                     var movieHtml = '<div class="movie" data-id="' + response.id + '">';
+//                     movieHtml += '<h3>' + response.title + '</h3>';
+//                     movieHtml += '<p>Rating: ' + response.rating + '</p>';
+//                     movieHtml += '<button class="edit-movie">Edit</button>';
+//                     movieHtml += '<button class="delete-movie">Delete</button>';
+//                     movieHtml += '</div>';
+//                     $('#movies-list').append(movieHtml);
+//                 }
+//             })
+//         })
+//     })
+// }
+//
+//
+//
+//
+//
+//
 
 
 
