57f045edddd6d33218cbb4e5c94fcfe6aee6771a
Jim Olson <jim.olson.456@gmail.com>
2023-04-13 16:08:28 -0500

    apr 13th 4:30

--- moviejs.js
+++ moviejs.js
@@ -5,26 +5,47 @@ fetch("http://localhost:3000/movies")
         // sets all the data we get back into html list
         let moviesHTML = movieData.map(movie =>{
             return `
+
+<!--<div>-->
+<!--<img src="https://www.istockphoto.com/vector/movie-time-vector-illustration-cinema-poster-concept-on-red-round-background-gm911590226-250985657">-->
+<!--</div>-->
+
+
 <div class="card">
             <div class="float-container">
                 <div class="column">
             <div class="cards"></div>
             <div class="card-title">
             <h3>Movie Name & Rating</h3>
-
+            
+        <label for="edit-movie-rating">Rating:</label>
+        <input type="number" id="edit-movie-rating" name="rating" min="1" max="10">
+        <button type="submit" id="edit-btn" data-editId=${movie.id}>Edit/Save</button>
 <div class="card-body">
 </div>
+
 <p>${movie.title}</p>
 <p>${movie.rating}</p>
+<button id="deletebutton" data-deleteId=${movie.id}>delete</button>
             </div>
         </div>
        </div>
     </div>
-</div>`
+</div>
+`
+
         })
 // from that list, I will append/concat this html form snippet to my pre-existing html(the moviesHTML variable turns into a string by using the join method)
         moviesHTML = `<div id="movies-list"></div>
+
+
+  
+ 
+<section> 
+<div class="content"> 
 <div id="add-movie-form">
+
+    
     <h2>Add a New Movie</h2>
     <form>
         <label for="title">Title:</label>
@@ -34,16 +55,9 @@ fetch("http://localhost:3000/movies")
         <button type="submit" id="add-btn">Add Movie</button>
     </form>
 </div>
-<div id="edit-movie-form">
-    <h2>Edit Movie</h2>
-    <form>
-        <input type="hidden" id="edit-movie-id" name="id">
-        <label for="edit-movie-title">Title:</label>
-        <input type="text" id="edit-movie-title" name="title">
-        <label for="edit-movie-rating">Rating:</label>
-        <input type="number" id="edit-movie-rating" name="rating" min="1" max="10">
-        <button type="submit" id="edit-btn">Save Changes</button>
-    </form>
+
+</section>
+ 
 </div>` + moviesHTML.join("")
         // set my html with the id of movie-content equal to moviesHTML
         document.getElementById("movie-content")
@@ -66,8 +80,10 @@ function deleteMovie(id) {
         .then(data => console.log(data))
         .catch (error => console.log(error));
 }
-
-// deleteMovie(11)
+//1. Make event listener
+//2. grab the dataAttribute so that I can pass that id into my function
+//ex: Number(deleteBtn.getAttribute("data-deleteId"));
+ deleteMovie(deleteBtn.getAttribute("data-deleteId"))
 setTimeout(function (){
     const addMovieButton = $('#add-btn');
 // const addMovieButton = document.querySelector('#add-btn');
@@ -107,7 +123,47 @@ setTimeout(function (){
 
 
 
+setTimeout(function (){
+    const editMovieButton = $('#edit-btn');
+// const addMovieButton = document.querySelector('#add-btn');
+    console.log(editMovieButton)
+    editMovieButton.click(function(event) {
 
+        event.preventDefault();
+
+
+        const title = document.querySelector('#edit-movie-title').value;
+        const rating = document.querySelector('#edit-movie-rating').value;
+        console.log(title)
+
+
+        const editMovie = {
+            title: title,
+            rating: rating
+        };
+        //post request(fetch)
+        fetch(`http://localhost:3000/movies/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(editMovie)
+        })
+            .then(response => response.json())
+            .then(movieData => {
+                console.log(movieData);
+                location.reload();
+
+            })
+            .catch(error => console.log(error));
+    });
+}, 1500)
+
+
+//1. put edit form each movie card
+//2. on your edit and delete button, put a !data attribute! that will contain the id of the movie you want to delete or edit
+//3. make event listener for delete and edit movie
+//4. get data attribute using grabAttribute method to grab the id of the movie so that you can edit and delete a movie
 
 
 
